48a601ef7099c2670b3c9525b6fa6fa23d3a171f
Abhay Sahoo <56788650+abhaysahoo@users.noreply.github.com>
2024-02-23 13:10:14 +0530

    commit initial files

---
+++ index.js
@@ -0,0 +1,18 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let password1El = document.getElementById("password-1")
+let password2El = document.getElementById("password-2")
+
+function generatePasswords() {
+    let password1 = ""
+    let password2 = ""
+    
+    for(let i = 0; i < 15; i++) {
+        password1 += characters[ Math.floor( Math.random() * characters.length ) ]
+        password2 += characters[ Math.floor( Math.random() * characters.length ) ]
+    }
+    
+    password1El.textContent = password1
+    password2El.textContent = password2
+}
